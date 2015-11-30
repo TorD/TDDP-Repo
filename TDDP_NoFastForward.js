7d2b623245060883693551227cf20142e38e128f
TorD <tor.damian@gmail.com>
2015-11-30 19:00:39 +0100

    NoFastForward bumped to 2.0.0
    You can now disable fast-forward for both Move Routes and Show Dialogue
    globally. Additionally a new Plugin Command has been added that lets you
    disable individual Show Text

--- TDDP_NoFastForward.js
+++ TDDP_NoFastForward.js
@@ -1,10 +1,20 @@
 //=============================================================================
 // TDDP_NoFastForward
-// Version: 1.0.0
+// Version: 2.0.0
 //=============================================================================
+var Imported = Imported || {};
+Imported.TDDP_NoFastForward = "2.0.0";
 /*:
- * @plugindesc 1.0.0 Disables the ability to fast forward move routes.
+ * @plugindesc 2.0.0 Disables the ability to fast forward move routes and/or text.                                                          id:TDDP_NoFastForward
  * @author Tor Damian Design / Galenmereth
+ *
+ * @param Disable for Move Routes
+ * @desc If set to true this will disable fast-forwarding for Move Routes globally. Default is false.
+ * @default false
+ *
+ * @param Disable for Show Text
+ * @desc If set to true this will disable fast-forwarding for Show Text globally. Default is false.
+ * @default false
  *
  * @help =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
  * Information
@@ -21,6 +31,76 @@
  * This plugin is free for both non-commercial and commercial use. Please see
  * http://mvplugins.tordamian.com/terms-of-use for the full terms of use.
  */
-Scene_Map.prototype.isFastForward = function() {
-    return false;
-};
+TDDP_NoFastForward = {};
+(function($) {
+    //=============================================================================
+    // Setting up parameters and internal funcs. All bound to TDDP_NoFastForward object
+    //=============================================================================
+    $.parameters               = $plugins.filter(function(p){return p.description.contains("id:TDDP_NoFastForward")})[0].parameters;
+    $.disableGlobalMoveRouteFf = Boolean($.parameters['Disable for Move Routes'] === 'true' || false);
+    $.disableGlobalTextFf      = Boolean($.parameters['Disable for Show Text']   === 'true' || false);
+    /**
+    * Disable text Fast Forwarding
+    * @method disableTextFf
+    */
+    $.disableTextFf = function() {
+        this._textFfDisabled = true;
+    }
+    /**
+    * Enable text Fast Forwarding
+    * @method enableTextFf
+    */
+    $.enableTextFf = function() {
+        this._textFfDisabled = false;
+    }
+    /**
+    * Get if text Fast Forwarding is disabled
+    * @method isTextFfDisabled
+    */
+    $.isTextFfDisabled = function() {
+        if (this._textFfDisabled || $.disableGlobalTextFf) return true;
+        return false;
+    }
+    //=============================================================================
+    // Game_Interpreter - register plugin commands
+    //=============================================================================
+    /**
+    * @CHANGED Adding DisableTextFF command
+    */
+    var _Game_Interpreter_pluginCommand = Game_Interpreter.prototype.pluginCommand;
+    Game_Interpreter.prototype.pluginCommand = function(command, args) {
+        _Game_Interpreter_pluginCommand.call(this, command, args)
+        if (command.toUpperCase() === 'DISABLETEXTFF') $.disableTextFf();
+    };
+    //=============================================================================
+    // Scene_Map
+    //=============================================================================
+    /**
+    * @CHANGED Adding support for disabling move route fast forward
+    */
+    Scene_Map_prototype_isFastForward = Scene_Map.prototype.isFastForward;
+    Scene_Map.prototype.isFastForward = function() {
+        if ($.disableGlobalMoveRouteFf) return false;
+        return Scene_Map_prototype_isFastForward.call(this);
+    };
+    //=============================================================================
+    // Window_Message
+    //=============================================================================
+    /**
+    * @CHANGED Extending to reset _noFastForward flag on new page
+    */
+    Window_Message_prototype_onEndOfText = Window_Message.prototype.onEndOfText;
+    Window_Message.prototype.onEndOfText = function() {
+        $.enableTextFf(); // Re-enable local text FF
+        Window_Message_prototype_onEndOfText.call(this);
+    };
+
+    /**
+    * @CHANGED Supports disabling text fast forward
+    */
+    var Window_Message_prototype_updateShowFast = Window_Message.prototype.updateShowFast;
+    Window_Message.prototype.updateShowFast = function() {
+        if ($.isTextFfDisabled()) return false;
+        return Window_Message_prototype_updateShowFast.call(this);
+    };
+})(TDDP_NoFastForward);
