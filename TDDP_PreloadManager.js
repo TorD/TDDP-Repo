93867a2391ee27dfaf3202c5460a918e8ca5b3d4
Tor Damian <tor.damian@gmail.com>
2022-07-18 08:56:55 +0200

    Update TDDP_PreloadManager.js
    Terms

--- TDDP_PreloadManager.js
+++ TDDP_PreloadManager.js
@@ -79,11 +79,32 @@ TDDP.PreloadManager = TDDP.PreloadManager || {};
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  * 3. Terms & Conditions
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * This plugin is free for both non-commercial and commercial use. Please see
- * http://mvplugins.tordamian.com/terms-of-use for the full terms of use.
+ * This plugin is free for both non-commercial and commercial use.
  *
  * A big thank you to Degica for making this plugin free for commercial use for
  * everyone!
+ *
+ * MIT License
+ *
+ * Copyright (c) 2019 Tor Damian Design
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 /*
 * =============================================================================
